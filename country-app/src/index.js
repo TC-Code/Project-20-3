a5b085af1e44b727505fb4f6d93c24140f964e73
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-05-20 18:43:59 +0200

    Update project. Add Redux DevTools.

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { getCountries } from "./actions/actions-countries";
+import DevTools from "./DevTools";
 
-render(<h1>Inicjalizacja projektu</h1>, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <div>
+      <h1>Inicjalizacja projektu</h1>
+      <DevTools />
+    </div>
+  </Provider>,
+  document.getElementById("root")
+);
+
+store.dispatch(getCountries());
